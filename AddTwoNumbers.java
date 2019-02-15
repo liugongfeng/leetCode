7cd2731802f9b1d9c054de589702aac07a27048a
liugongfeng <liugong.feng@aliyun.com>
2019-02-15 11:06:36 +0800

    Add two numbers

--- AddTwoNumbers.java
+++ AddTwoNumbers.java
@@ -1,35 +1,44 @@
-//import java.util.List;
-//
-//public class AddTwoNumbers {
-///** Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
-// *  Output: (7 -> 0 -> 8)
-// *
-// * ***/
-//
-//    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-//        ListNode dummy = ListNode(0);
-//        int sum = 0;
-//        ListNode current = dummy;
-//        ListNode p1 = l1,  p2 = l2;
-//
-//        while (p1 != null || p2 != null) {
-//            if (p1 != null) {
-//                sum += p1.val;
-//                p1 = p1.next;
-//            }
-//            if (p2 != null) {
-//                sum += p2.val;
-//                p2 = p2.next;
-//            }
-//            current.next = new ListNode(sum % 10);
-//            sum = sum / 10;
-//            current = current.next;
-//        }
-//        if (sum == 1)
-//            current.next = new ListNode(1);
-//
-//        return dummy.next;
-//    }
-//
-//
-//}
+
+
+public class AddTwoNumbers {
+/** Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+ *  Output: (7 -> 0 -> 8)
+ *
+ * ***/
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummy = ListNode(0);
+        int sum = 0;
+        ListNode current = dummy;
+        ListNode p1 = l1,  p2 = l2;
+
+        while (p1 != null || p2 != null) {
+            if (p1 != null) {
+                sum += p1.val;
+                p1 = p1.next;
+            }
+            if (p2 != null) {
+                sum += p2.val;
+                p2 = p2.next;
+            }
+            current.next = new ListNode(sum % 10);
+            sum = sum / 10;
+            current = current.next;
+        }
+        if (sum == 1)
+            current.next = new ListNode(1);
+
+        return dummy.next;
+    }
+
+
+    public class ListNode {
+        private int val;
+        private ListNode next;
+
+        public ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+}
