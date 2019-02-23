7a439e90d038ca6676e69459c2ae0539f21b821f
liugongfeng <liugong.feng@aliyun.com>
2019-02-23 18:11:27 +0800

    generateParenthese

---
+++ LeetCode 22.py
@@ -0,0 +1,24 @@
+class Solution:
+
+    def generateParenthesis(self, n):
+        """
+        :param n: int
+        :return: List[str]
+        """
+        if n == 0:
+            return []
+
+        result = []
+        self.helper(n, n, '', result)
+        return result
+
+    def helper(self, l, r, item, result):
+        if l > r:
+            return
+        if l == 0 and r == 0:
+            result.append(item)
+        if l > 0:
+            self.helper(l - 1, r, item + '(', result)
+        if r > 0:
+            self.helper(l, r - 1, item + ')', result)
+
