f180e501c0c4b7a9e3a0232eb72255061bc63f25
liugongfeng <liugong.feng@aliyun.com>
2019-03-02 22:39:41 +0800

    Add Two numbers

---
+++ LeetCode 2.py
@@ -0,0 +1,48 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def addTwoNumbers(self, l1, l2):
+        """
+        :param l1: ListNode
+        :param l2:  ListNode
+        :return:  ListNode
+        """
+        if l1 == None:
+            return l2
+        if l2 == None:
+            return l1
+
+        carry = 0
+        dummuy = ListNode(0)
+        p = dummuy
+
+        while l1 and l2:
+            p.next = ListNode((l1.val + l2.val + carry) % 10)
+            carry = (l1.val + l2.val + carry) // 10
+            l1 = l1.next
+            l2 = l2.next
+            p = p.next
+
+        if l2:
+            while l2:
+                p.next = ListNode((l2.val + carry) % 10)
+                carry = (l2.val + carry) // 10
+                l2 = l2.next
+                p = p.next
+
+        if l1:
+            while l1:
+                p.next = ListNode((l1.val + carry) % 10)
+                carry = (l1.val + carry) // 10
+                l1 = l1.next
+                p = p.next
+
+        if carry == 1:
+            p.next = ListNode(1)
+
+        return dummuy.next
+
