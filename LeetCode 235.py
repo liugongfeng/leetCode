bf91f2b5bf5a616a1dd40336458308e11aff7a2f
liugongfeng <liugong.feng@aliyun.com>
2019-03-08 23:16:48 +0800

    Lowest Common Ancestor of BST

---
+++ LeetCode 235.py
@@ -0,0 +1,24 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if p.val < root.val > q.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        if p.val > root.val < q.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        return root
+
+
+    def lowestCommonAncestor_2(self, root, p, q):
+        while root:
+            if p.val < root.val > q.val:
+                root = root.left
+            elif p.val > root.val < q.val:
+                root = root.right
+            else:
+                return root
