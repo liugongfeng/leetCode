5511ba7042def3a994e1142031f059d2c5a5476b
liugongfeng <liugong.feng@aliyun.com>
2019-03-28 16:21:35 +0800

    Update LeetCode 1.py

--- LeetCode 1.py
+++ LeetCode 1.py
@@ -3,29 +3,16 @@ Example:
     Input: nums = [2, 7, 1, 6]
     Return: table = {2:0, 7:1, 1:2, 6:3}
 """
-def createHashTable(nums):
-    table = {}
-    for i in range(len(nums)):
-        table[nums[i]] = i
-    return table
 
 """ Two-Pass Hash Table. """
 def twoSum(nums, target):
-    table = createHashTable(nums)
+    d = {}
     for i in range(len(nums)):
-        if target - nums[i] in table:
-            if table[target - nums[i]] != i:
-                return [i, table[target - nums[i]]]
-    return []
+       if target - nums[i] not in d:
+           d[nums[i]] = i
+       else:
+            return [d[target - nums[i]], i]
 
-
-""" One-Pass Hash Table. """
-def twoSumOnePath(nums, target):
-    table = createHashTable(nums)
-    for i, num in enumerate(nums):
-        if target - num in table:
-            return [i, table[target - num]]
-    return []
 
 
 if __name__ == "__main__":
