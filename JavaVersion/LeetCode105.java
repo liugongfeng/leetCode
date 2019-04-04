e77ee6bf87af585c0a18b662218d471044d6bb36
liugongfeng <liugong.feng@aliyun.com>
2019-04-04 13:48:07 +0800

    Invert Binary

---
+++ JavaVersion/LeetCode105.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class LeetCode105 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+}
