46d6726d8d722dc746717b13d88ab5183f29d749
liugongfeng <liugong.feng@aliyun.com>
2019-04-14 16:51:16 +0800

    Largest Number

---
+++ LeetCode 179.py
@@ -0,0 +1,8 @@
+from functools import cmp_to_key
+
+
+class Solution:
+    def PrintMinNumber(self, nums):
+        num = list(map(str, nums))
+        num.sort(key = cmp_to_key(lambda b, a: ((a+b)<(b+a))-((a+b)>(b+a)) ))
+        return ''.join(num).lstrip('0') or '0'
