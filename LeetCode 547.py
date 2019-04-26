1799bb52fae2800180c3e2e565f8a21bfa09aaa0
liugongfeng <liugong.feng@aliyun.com>
2019-04-26 21:38:02 +0800

    Friend Circles

--- LeetCode 547.py
+++ LeetCode 547.py
@@ -12,14 +12,37 @@ class Solution:
         return count
 
 
+    def DFS(self, M, visited, i):
+        for j in range(len(M)):
+            if M[i][j] == 1 and visited[j] == 0:
+                visited[j] = 1
+                self.DFS(M, visited, j)
 
-    def DFS(self, grid, x, y):
-        pass
+
+    """ Solution 2"""
+    def findCircleNum2(self, M):
+        def find(node):
+            if circles[node] == node:
+                return node
+            root = find(circles[node])
+            circles[node] = root
+            return root
+
+        n = len(M)
+        circles = {x: x for x in range(n)}
+        
+        for i in range(n):
+            for j in range(i, n):
+                if i != j and M[i][j]==1 and find(i) != find(j):
+                    circles[find(i)] = find(j)
+                    
+        return sum([1 for k,v in circles.items() if k==v])
+
 
 s = Solution()
 grid =[ [1,1,0],
         [1,1,0],
         [0,0,1] ]
 
-res = s.findCircleNum(grid)
+res = s.findCircleNum2(grid)
 print(res)
